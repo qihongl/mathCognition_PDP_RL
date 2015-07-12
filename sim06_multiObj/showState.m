1f3c64a14b537c20af2ae67ada597f021ebc6de5
Qihong Lu <lvqihong1992@gmail.com>
2015-07-12 13:33:39 -0700

    small changes

--- sim06_multiObj/showState.m
+++ sim06_multiObj/showState.m
@@ -1,15 +1,15 @@
 % written by professor Jay McClelland
 function [ ] = showState( )
-global p w d a h;
+global p w d a;
 
 axes(d.rax);
 plot(w.rS.time,a.Rwd,'-*'); hold on;
 ylim(d.rax,[-0.1 1.1]); xlim(d.rax,[-0.25,w.rS.time+0.25]);
 
 t = w.rS.time;
-%plot the history of eye and hand positions
+% plot the history of eye and hand positions
 axes(d.hax);
-plot(d.hax,[-50 50],[t t]); hold on;
+plot(d.hax,[-p.spRad p.spRad],[t t]); hold on;
 ylim(d.hax,[-0.25 t + .25]);
 text(w.rS.eyePos,t,'@','HorizontalAlignment','center');
 text(w.rS.handPos,t,'#','HorizontalAlignment','center');
@@ -22,6 +22,6 @@ for i = 1 : p.nItems
     text(w.rS.targPos(i),t,symbol,'HorizontalAlignment','center');
 end
 axes(d.wax);
-plot(-50:50,a.wts);
+plot(-p.spRad:p.spRad,a.wts);
 end
 
