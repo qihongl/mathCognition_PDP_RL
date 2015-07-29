12bae5fe978041fd4c178cc2e56dbd2680e191f6
Qihong Lu <lvqihong1992@gmail.com>
2015-07-29 12:43:24 -0700

    new teacher forcing

---
+++ sim14_forcing+/showState.m
@@ -0,0 +1,43 @@
+% written by professor Jay McClelland
+function [ ] = showState( )
+global p w d a;
+% plot rewards over time 
+axes(d.rwdAx);
+plot(w.rS.time,a.Rwd,'-*'); hold on;
+ylim(d.rwdAx,[p.r.bigNeg p.r.bigPos]); 
+xlim(d.rwdAx,[-0.25,w.rS.time+0.25]);
+
+% plot the history of eye and hand positions
+t = w.rS.time;
+axes(d.historyAx);
+plot(d.historyAx,[-p.spRad p.spRad],[t t]); hold on;
+ylim(d.historyAx,[-0.25 t + .25]);
+if w.done
+    text(w.rS.handPos,t,'o','HorizontalAlignment','center');
+else
+    text(w.rS.eyePos,t,'@','HorizontalAlignment','center');
+    text(w.rS.handPos,t,'#','HorizontalAlignment','center');
+end
+
+for i = 1 : w.nItems
+    if w.rS.targRemain(i)
+        symbol = 'X';
+    else
+        symbol = '|';
+    end
+    text(w.rS.targPos(i),t,symbol,'HorizontalAlignment','center');
+end
+
+% plot all weights
+axes(d.allWeightsAx);
+plot(-p.eyeRad:p.eyeRad,a.wts);
+
+% plot weights around fovea
+plotRange = 4; 
+axes(d.someWeightsAx)
+midIdx = (p.mvRad - plotRange) : (p.mvRad + plotRange);
+plot((a.wts(midIdx,:))')
+legend_str = num2str((midIdx-p.mvRad)');
+legend(legend_str,'location','NorthWest')
+end
+
---
+++ sim14_forcing+/trainAgent.m
@@ -0,0 +1,37 @@
+% written by professor Jay McClelland
+function [record] = trainAgent(epoch)
+%% This function trains the network n trials
+% initialize parameters
+global p a w;
+initParams(epoch);
+
+% preallocate
+record.a = cell(1,epoch);
+record.s.steps = nan(1,epoch);
+record.s.indices = cell(1,epoch);
+record.s.completed = false(1,epoch);
+record.s.numItemsShowed = zeros(1,epoch);
+
+% train the model for n trials
+fprintf('%s\n', pwd);
+fprintf('Start training for %d epochs\n', epoch);
+for i = 1:p.runs
+    fprintf('%d\n', i )
+    %% run the model 
+    result = runAgent();
+    % increment the softmax scaling factor
+    if a.smgain < p.smi_upperLim
+        a.smgain = a.smgain + p.smirate;
+    end
+    % save performance
+    record.s.steps(i) = result.steps;
+    record.s.indices{i} = result.indices;
+    record.s.numItemsShowed(i) = w.nItems;
+    if w.nItems == length(getNonzeros(record.s.indices{i}))
+        record.s.completed(i) = true;
+    end
+end
+% save parameters
+record.a = result.a;
+record.p = p;
+end
