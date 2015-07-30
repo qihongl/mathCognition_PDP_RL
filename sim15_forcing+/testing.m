05946fa80be8471b5e58079652a4f46eea59b370
Qihong Lu <lvqihong1992@gmail.com>
2015-07-30 09:55:18 -0700

    exp with teacher forcing

---
+++ sim15_forcing+/testing.m
@@ -0,0 +1,14 @@
+% just testing, a short cut for running the model
+function testing(epoch)
+if nargin == 0
+    epoch = 5000;
+end
+record = trainAgent(epoch);
+save('record','record');
+
+% eval the performance
+quiz()
+checkLearning()
+
+beep % notice me that the program was ended
+end
