a79ad720d82067e42eb6d878a7c78f9f636e5a13
Qihong Lu <lvqihong1992@gmail.com>
2015-07-30 11:38:23 -0700

    performance by cardinality

---
+++ testModel/getScoresByCard.m
@@ -0,0 +1,14 @@
+function [s] = getScoresByCard(record)
+% compute performance score for each cardinaility 
+%% initialization 
+global p plots;
+p = record.p;
+
+for i = 1 : p.runs
+    
+end
+
+
+
+end
+
