2b0388ab3573779b2324c98bc6abd770230ed0cb
Qihong Lu <lvqihong1992@gmail.com>
2015-07-30 09:52:50 -0700

    run n models

--- sim14_cdf/testGroup.m
+++ sim14_cdf/testGroup.m
@@ -1,19 +1,12 @@
 function [] = testGroup(nSubj, epoch)
 %testGroup Run N models, to establish sample size
 if nargin == 0
-    epoch = 5;
+    epoch = 5000;
     nSubj = 10; 
 end
 
 %% create a new directory to save the results
-index = 0;
-dirName = sprintf('%s%.2d','groupResults', index);
-% if directory already exists, use a new name 
-while exist(dirName, 'dir') == 7
-    index = index + 1;
-    dirName = sprintf('%s%.2d','groupResults', index);
-end
-mkdir(dirName)
+dirName = createDir('groupData');
 
 %% run the group analysis  
 for i = 1:nSubj
@@ -21,6 +14,7 @@ for i = 1:nSubj
     filename = sprintf('record%.2d', i);
     save([dirName '/'  filename],'record');
 end
-% beep % notice me that the program was ended
+
+beep
 end
 
