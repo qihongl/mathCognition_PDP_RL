a07b0b07e451ef21f655c3d643539317eb733f0e
Qihong Lu <lvqihong1992@gmail.com>
2015-07-30 15:26:53 -0700

    in prog

---
+++ testModel/gpCheckLearning.m
@@ -0,0 +1,15 @@
+%% Check the learning curve of a group of models, get average performance
+clear all; 
+
+%% Get data from the current directory 
+dataDir = {'groupData00', 'groupData01'};
+filename = 'record.mat';
+load([pwd '/' filename], 'record')
+
+
+
+
+
+
+
+
