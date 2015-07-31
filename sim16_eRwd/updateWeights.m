5471c1729fb9038a9f00b405b030288e3e2870db
Qihong Lu <lvqihong1992@gmail.com>
2015-07-31 13:17:01 -0700

    implemented df rwd

--- sim16_eRwd/updateWeights.m
+++ sim16_eRwd/updateWeights.m
@@ -4,17 +4,17 @@ function [ ] = updateWeights()
 % 1. the reward policy
 % 2. the weight update
 % 3. activate the "teaching"
-global a w p;
+global p a w;
 %% compute the reward values according to the reward policy
-Rwd = computeRwd();
-
+curRwd = computeRwd();
+expRwd = max(a.wts*w.vS.visInput');
 %% assign the reward values
-a.Rwd = Rwd;
-% a.dfRwd = Rwd * p.gamma^w.rS.td;
+% a.Rwd = curRwd;
+a.dfRwd = curRwd + expRwd * p.gamma;
 
 %% update the weights
 % change in weights equals input times reward prediction error
-inc = p.lrate*(a.Rwd - a.act(a.choice));          % TODO consider the update rule
+inc = p.lrate*(a.dfRwd - a.act(a.choice));          % TODO consider the update rule
 a.wts(a.choice,:) = a.wts(a.choice,:) + inc*w.vS.oldInput;
 
 %% start over if an incorrect action was made
