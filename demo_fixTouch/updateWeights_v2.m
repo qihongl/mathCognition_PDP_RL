5f63ef71370cabe7020c093270d0fa0a45970588
Qihong Lu <lvqihong1992@gmail.com>
2015-07-31 11:02:15 -0700

    new weight update for fixTouch model

--- demo_fixTouch/updateWeights_v2.m
+++ demo_fixTouch/updateWeights_v2.m
@@ -1,16 +1,21 @@
 % written by professor Jay McClelland
+% not currently used - this version adds the actual reward and disc future
+% reward rather than using the actual if obtained and disc future rwd
+% otherwise
 function [ ] = updateWeights( )
 
 global a w p;
 % change in weights equals input times reward prediction error
 if w.rS.handPos == w.rS.targPos
     aRwd = 1; %was Rwd  actual reward
-% else
+% else %TOOK NEXT LINE OUT of else
 %     Rwd = max(a.wts*w.vS.visInput');
 end
 eRwd = max(a.wts*w.vS.visInput');
-a.Rwd = aRwd;
-a.dfRwd = (aRwd+eRwd)*p.gamma^w.rS.td; %this modulates the actual reward by the delay - not clear if this is optimal!
+a.Rwd = aRwd; %was = Rwd
+%next line is changed:
+%This version modulates the actual reward by the delay - not clear if this is optimal!
+a.dfRwd = (aRwd+eRwd)*p.gamma^w.rS.td;
 % isize = min(abs(a.dfRwd - a.act(a.choice)),.01);
 % isign = sign(a.dfRwd - a.act(a.choice));
 % inc = p.lrate*isize*isign;
