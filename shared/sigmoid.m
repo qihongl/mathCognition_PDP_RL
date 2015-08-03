7a75ac6b4c538b11d076cdcf737710cd36df2016
Qihong Lu <lvqihong1992@gmail.com>
2015-08-03 16:46:18 -0700

    bound the output

--- shared/sigmoid.m
+++ shared/sigmoid.m
@@ -1,6 +1,12 @@
-function g = sigmoid(z)
+function output = sigmoid(z)
 %SIGMOID Compute sigmoid functoon
 %   J = SIGMOID(z) computes the sigmoid of z.
 
-g = 1.0 ./ (1.0 + exp(-z));
+output = 1.0 ./ (1.0 + exp(-z));
+
+% bound the output 
+upperBound = 0.9998;
+lowerBound = 0.0002;
+output(output > upperBound) = upperBound;
+output(output > lowerBound) = lowerBound;
 end
