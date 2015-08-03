eb123cd4b3699406fc0cc563a94319bf7fb3068d
Qihong Lu <lvqihong1992@gmail.com>
2015-08-03 13:11:29 -0700

    plot hidden weights

---
+++ testModel/showWeights2.m
@@ -0,0 +1,24 @@
+function [ ] = showWeights( )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+global p w d a;
+FONTSIZE = 13; 
+
+% plot all weights
+axes(d.allWeights);
+imagesc(a.wts1)
+% imagesc(a.wtsTest)
+title(d.allWeights, 'weights - from visual to hidden layer', 'fontsize', FONTSIZE)
+ylabel(d.allWeights, 'hidden units', 'fontsize', FONTSIZE);
+xlabel(d.allWeights, 'visual input', 'fontsize', FONTSIZE)
+colorbar
+
+% plot weights around fovea
+axes(d.heatMap)
+imagesc(a.wts2)
+title(d.heatMap, 'weights - from hidden to action layer', 'fontsize', FONTSIZE)
+xlabel(d.heatMap, 'hidden units', 'fontsize', FONTSIZE)
+ylabel(d.heatMap, 'actions units', 'fontsize', FONTSIZE)
+colorbar
+end
+
