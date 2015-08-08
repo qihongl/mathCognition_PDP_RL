a01c5bae911b5f6569c0d22efeb443e79270951b
Qihong Lu <lvqihong1992@gmail.com>
2015-08-07 17:06:43 -0700

    modify save dir

--- sim16_eRwd/getSaveDir.m
+++ sim16_eRwd/getSaveDir.m
@@ -1,15 +1,23 @@
-function [ saveName ] = getSaveDir(  )
+function [ saveName ] = getSaveDir(dataFolderName, dataName)
 % create a directory to save the simulation data
 % the directory will be in the format of 'sim001'
+if nargin == 0 
+    dataFolderName = 'data';
+    dataName = 'sim';
+end
+
+% create a data dir
+if exist(dataFolderName) ~= 7
+    mkdir(dataFolderName);
+end
 
 % set the default name 
-defaultName = 'sim';
 index = 1;
-saveName = sprintf('%s%.3d', defaultName, index);
+saveName = sprintf('%s/%s%.3d', dataFolderName, dataName, index);
 % increment the index until an unused index was found 
 while exist(saveName) == 7
     index = index + 1; 
-    saveName = sprintf('%s%.3d', defaultName, index);
+    saveName = sprintf('%s/%s%.3d',dataFolderName , dataName, index);
 end
 
 % make the directory
