f2271d500d3ef289e793bbafc138775db6381b37
Qihong Lu <lvqihong1992@gmail.com>
2015-08-09 00:12:06 -0700

    some changes

---
+++ testGroup/gpQuiz.m
@@ -0,0 +1,40 @@
+%% Test a group of models, in order to establish average performance
+clear all; clear global;
+
+%% Get data from the current directory
+
+nSubj = 5;
+numQs = 50;
+nGroups = 8;
+name = 'record';
+dirName = 'groupData';
+
+for i = 0 : nGroups
+    % get access to the right data directory 
+    dataDir = sprintf('%s%.2d',dirName, i);
+    datapath = [pwd '/' dataDir '/'];
+    addpath(pwd);
+    
+    %% Single subject analysis
+    % loop over all directories
+    filenames = getFilenames(name, nSubj);
+    groupScores = cell(1, nSubj);
+    for n = 1 : nSubj
+        % load the record for all subjects
+        fullPath = [datapath filenames{n}];
+        load(fullPath, 'record');
+        loadParams(record);
+        % do single subject analysis and save the results
+        scores = askQuesitons(numQs, false);
+        groupScores{n} = evaluateModel(scores, numQs);
+        clear global;
+    end
+    rmpath(pwd);
+    
+    
+    %% save the results
+    saveFileName = sprintf('%sgroupScores' ,datapath);
+    save(saveFileName,'groupScores');
+    
+end
+beep;
