943eaac865a9d647cf43930a4f0439dbacf5ec58
Qihong Lu <lvqihong1992@gmail.com>
2015-08-17 17:37:26 -0500

    implement completing unit

---
+++ sim16.5_compleUnit/initPlot.m
@@ -0,0 +1,20 @@
+function initPlot()
+% Set up for the plot 
+global d
+%% set up the figure
+d.fh = figure();
+d.fh.WindowStyle = 'docked';
+% name of the plots
+d.rwd = subplot(2,2,1);
+d.history = subplot(2,2,2);
+d.lineWts = subplot(2,2,3);
+d.heatWts = subplot(2,2,4);
+
+% constant
+d.FONTSIZE = 14; 
+
+% % plot
+axes(d.history); cla;
+axes(d.rwd); cla;
+end
+
