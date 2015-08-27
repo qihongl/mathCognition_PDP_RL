6edbe053cd46362f9b358946580ce592a1ae9836
Qihong Lu <lvqihong1992@gmail.com>
2015-08-27 15:15:10 -0500

    modularize the code

--- testModel/evaluateModel_quiz.m
+++ testModel/evaluateModel_quiz.m
@@ -1,79 +1,124 @@
 function [finalScore] = evaluateModel_quiz( score, numQs )
-%UNTITLED2 Summary of this function goes here
 %   This function assume the data is the quiz data
 %   Namely, the input data can be converted to "cardinality X numQs matrix"
 
 global p;
-numTotalQ = numel(score);
+if numel(score) ~= p.maxItems * numQs
+    error('Size of the data is strange');
+end
 
 % preallocate
-steps = nan(p.maxItems, numQs);
-orders = cell(p.maxItems, numQs);
-numSkips = nan(p.maxItems, numQs);
-completed = nan(p.maxItems, numQs);
-numItemsShowed = nan(p.maxItems, numQs);
-numErrors = nan(p.maxItems, numQs);
-orderCorrect = false(p.maxItems, numQs);
-stopEarly = false(p.maxItems, numQs);
+s = resourcePreallocation(numQs);
 
 %% save data in "cardinality X numQs" form
 for cardinality = 1 : p.maxItems
     for i = 1 : numQs
         tempScore = score{cardinality,i};
-        steps(cardinality, i) = tempScore.steps;
-        orders{cardinality, i} = getNonzeros(tempScore.indices);
-        if any(orders{cardinality,i} == p.maxItems +1) && length(orders{cardinality,i}) -1  < cardinality
-            stopEarly(cardinality, i) = true; 
+        s.steps(cardinality, i) = tempScore.steps;
+        s.orders{cardinality, i} = getNonzeros(tempScore.indices);
+        if any(s.orders{cardinality,i} == p.maxItems +1) && length(s.orders{cardinality,i}) -1  < cardinality
+            s.stopEarly(cardinality, i) = true;
         end
-        % delete "finish" unit 
-        orders{cardinality, i} = orders{cardinality, i}(orders{cardinality, i} ~= p.maxItems + 1);
-        numSkips(cardinality, i) = detectSkip(orders{cardinality, i});
-        completed(cardinality, i) = tempScore.completed;
-        numItemsShowed(cardinality, i) = tempScore.nItemsShowed;
-        numErrors(cardinality, i) = tempScore.numErrors;
+        % delete "finish" unit
+        s.orders{cardinality, i} = s.orders{cardinality, i}(s.orders{cardinality, i} ~= p.maxItems + 1);
+        s.numSkips(cardinality, i) = detectSkip(s.orders{cardinality, i});
+        s.completed(cardinality, i) = tempScore.completed;
+        s.numItemsShowed(cardinality, i) = tempScore.nItemsShowed;
+        s.numErrors(cardinality, i) = tempScore.numErrors;
+        s.numSkips2(cardinality, i) = tempScore.numSkips;
+        s.numDoubleTouch(cardinality, i) = tempScore.numDoubleTouch;
         if ismonotonic(getNonzeros(tempScore.indices))
-            orderCorrect(cardinality, i) = true;
+            s.orderCorrect(cardinality, i) = true;
         end
     end
     
 end
 
-
-%% 1. compute summary statistics (combine all cardinality)
-% average number of steps used
-overall.meanSteps = mean(steps(:));
-% don't go right to left
-overall.monotonicRate = sum(orderCorrect(:))/ numTotalQ;
-% don't skip item
-overall.skipRate = 1 - (sum(numSkips(:) == 0) / numTotalQ);
-% how many items were skipped
-overall.meanNumSkips = mean(numSkips(:));
-% whether touch all items within 100 steps
-overall.completeRate = sum(completed(:))/numTotalQ;
-overall.meanNumErrors = mean(numErrors(:));
-% complete without making error
-correctCompletedMatrix = bsxfun(@and, numErrors == 0, completed);
-overall.correctCompRate = sum(sum(correctCompletedMatrix))/ numTotalQ;
-% stop early rate
-overall.stopEarlyRate = sum(stopEarly(:)) / numTotalQ;
-
-%% 2. compute summary statistics (for every cardinality)
-byCard.meanSteps = mean(steps,2);
-byCard.monotonicRate = sum(orderCorrect,2)./numQs;
-byCard.skipRate = (numQs-sum(numSkips == 0,2))./numQs;
-byCard.meanNumSkips = mean(numSkips,2);
-byCard.compRate = sum(completed,2)./numQs;
-byCard.correctCompRate = sum(correctCompletedMatrix,2)./numQs;
-byCard.meanNumErrors = mean(numErrors,2);
-byCard.stopEarlyRate = sum(stopEarly,2) / numQs;
+% get data for overall performance and data separated by cardinality 
+[overall, byCard] = getData(s, numQs);
 
 % check if the distribution of caridnality is the same
-if any(mean(numItemsShowed,2) ~= (1 : p.maxItems)')
+if any(mean(s.numItemsShowed,2) ~= (1 : p.maxItems)')
     error('cardinality distribution is not uniform.');
 end
 % now we can assume each caridnality were tested equally
 
 %% print results
+printResultsToConsole(numQs, overall, byCard)
+
+%% save data
+finalScore.overall = overall;
+finalScore.byCard = byCard;
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%% Some helper functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% preallocate for scores
+function [s] = resourcePreallocation(numQs)
+global p; 
+
+% number of total questions 
+s.numTotalQ = p.maxItems * numQs;
+
+% preallocate some space for saving the results
+s.steps = nan(p.maxItems, numQs);
+s.orders = cell(p.maxItems, numQs);
+s.completed = nan(p.maxItems, numQs);
+s.numItemsShowed = nan(p.maxItems, numQs);
+s.numErrors = nan(p.maxItems, numQs);
+s.numSkips = nan(p.maxItems, numQs);
+s.numSkips2 = nan(p.maxItems, numQs);
+s.numDoubleTouch = nan(p.maxItems, numQs);
+s.orderCorrect = false(p.maxItems, numQs);
+s.stopEarly = false(p.maxItems, numQs);
+end
+
+%% get data for overall performance and data separated by cardinality 
+function [overall, byCard] = getData(s, numQs)
+
+%% 1. compute summary statistics (combine all cardinality)
+% average number of steps used
+overall.meanSteps = mean(s.steps(:));
+% don't go right to left
+overall.monotonicRate = sum(s.orderCorrect(:))/ s.numTotalQ;
+% don't skip item
+overall.skipRate = 1 - (sum(s.numSkips(:) == 0) / s.numTotalQ);
+% how many items were skipped
+overall.meanNumSkips = mean(s.numSkips(:));
+overall.meanNumSkips2 = mean(s.numSkips2(:)); % second way of computing numSkips
+% times of double touching
+overall.meanNumDoubleTouch = mean(s.numDoubleTouch(:));
+% whether touch all items within 100 steps
+overall.completeRate = sum(s.completed(:))/s.numTotalQ;
+overall.meanNumErrors = mean(s.numErrors(:));
+% complete without making error
+correctCompletedMatrix = bsxfun(@and, s.numErrors == 0, s.completed);
+overall.correctCompRate = sum(sum(correctCompletedMatrix))/ s.numTotalQ;
+% stop early rate
+overall.stopEarlyRate = sum(s.stopEarly(:)) / s.numTotalQ;
+
+%% 2. compute summary statistics (for every cardinality)
+byCard.meanSteps = mean(s.steps,2);
+byCard.compRate = sum(s.completed,2)./numQs;
+byCard.correctCompRate = sum(correctCompletedMatrix,2)./numQs;
+byCard.monotonicRate = sum(s.orderCorrect,2)./numQs;
+byCard.skipRate = (numQs-sum(s.numSkips == 0,2))./numQs;
+byCard.meanNumSkips = mean(s.numSkips,2);
+byCard.meanNumSkips2 = mean(s.numSkips2,2);
+byCard.meanNumDoubleTouch = mean(s.numDoubleTouch,2);
+byCard.meanNumErrors = mean(s.numErrors,2);
+byCard.stopEarlyRate = sum(s.stopEarly,2) / numQs;
+
+end
+
+
+%% print the summary results
+function printResultsToConsole(numQs, overall, byCard)
+global p; 
+
 tt = sprintf('Performance on %d * %d questions\n', p.maxItems, numQs);
 fprintf('------------------------------------------------\n')
 fprintf('Directory: "%score"\n', pwd);
@@ -103,12 +148,4 @@ for n = 1 : p.maxItems
 end
 
 fprintf('------------------------------------------------\n')
-
-%% save data 
-finalScore.overall = overall; 
-finalScore.byCard = byCard; 
-
 end
-
-
-
