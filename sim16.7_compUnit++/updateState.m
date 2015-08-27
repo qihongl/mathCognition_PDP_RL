956ce85c328ddae86d40828a704a9035740cbe0c
Qihong Lu <lvqihong1992@gmail.com>
2015-08-27 16:54:18 -0500

    comment

--- sim16.7_compUnit++/updateState.m
+++ sim16.7_compUnit++/updateState.m
@@ -9,14 +9,16 @@ global w h p a;
 % the relative locations of eye and hand
 w.vS.eyePos = 0;
 w.vS.handPos = w.rS.handPos - w.rS.eyePos;
-% the next line is the apparent new position of the target
-% purturbed by scalar variability based on true targ pos
 
+%% adding noise to the locations of the obejcts
+% 1. iid noise for all objects 
 % w.vS.targPos = round((w.rS.targPos - w.rS.eyePos).*(1+randn(1,length(w.rS.targPos))*p.wf));
+% 2. same noise for all objects
 w.vS.targPos = round((w.rS.targPos - w.rS.eyePos)*(1 + randn*p.wf));
+% 3. no noise
 % w.vS.targPos = w.rS.targPos - w.rS.eyePos;
 
-% the error depends on the distance between target and fixation
+%% the error depends on the distance between target and fixation
 w.vS.sd = p.wf*abs(w.rS.targPos - w.rS.eyePos);
 w.vS.sd(w.vS.sd < p.wf) = p.wf;
 w.vS.sd(w.vS.targPos == 0) = 0.01;
