e295c678a585d14788cc7d617a9b6381f4b25e36
Qihong Lu <lvqihong1992@gmail.com>
2015-08-27 16:54:08 -0500

    delete comments

--- sim16.7_compUnit++/getVisualInput.m
+++ sim16.7_compUnit++/getVisualInput.m
@@ -2,22 +2,12 @@ function [cumVisualPattern] = getVisualInput()
 % this function generate cumulative input patterns for multiple objects
 global p w;
 
-% if w.vS.sd == 0
-%     error('Invalid Input: w.vS.sd cannot be zero!');
-% end
-
 visInput = nan(w.nItems, p.eyeRange);
 %% generate visual input for every item
 for i = 1 : w.nItems
     visualCDF = normcdf((-p.eyeRad:p.eyeRad+1) - .5, w.vS.targPos(i),w.vS.sd(i));
     % approximate the area of the rectangular region under PDF using its CDF
     visInput(i,:) = visualCDF(2:end) - visualCDF(1:length(visualCDF)-1);
-    
-    %     cdf_L = normcdf((-p.eyeRad:p.eyeRad) - .5, w.vS.targPos(i),w.vS.sd(i));
-    %     cdf_R = normcdf((-p.eyeRad:p.eyeRad) + .5, w.vS.targPos(i),w.vS.sd(i));
-    %     if visInput(i,:)~= cdf_R - cdf_L
-    %         error('what?!');
-    %     end
 end
 %% sum all visual input, columnwise, no normalization 
 cumVisualPattern = sum(visInput,1);
