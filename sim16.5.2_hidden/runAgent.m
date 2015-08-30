ebcc7e50c4fe3d2687e070df909ffb59905c43b9
Qihong Lu <lvqihong1992@gmail.com>
2015-08-30 13:37:47 -0500

    using hidden units

---
+++ sim16.5.2_hidden/runAgent.m
@@ -0,0 +1,67 @@
+% written by professor Jay McClelland
+function [ results ] = runAgent()
+global a w h p mode;
+
+%% initialize the state
+initState();
+updateState();
+computeAnswer();    % compute the true 'answers'
+
+%% training the model once
+i = 0;
+indices = zeros(1,p.maxIter);
+while ~(w.done) && i < p.maxIter
+    %% choose action
+    selectAction();
+    move();
+    %% update the state
+    updateState();
+    updateWeights();
+    indices(i + 1) = recordAction();     % record the "touch-index"
+    i = i+1;
+    %% teaching mode, executed when redo is needed
+    if p.teachingModeOn && mode.teach && w.redo
+        i = 0;
+        letTheModelTryAgain();
+    end
+end
+
+updateTeachingConditions();
+
+%% save result
+results.numErrors = w.errors;
+results.indices = indices;
+results.steps = i;
+results.h = h;
+results.a = a;
+end
+
+
+%% Helper functions
+
+function updateTeachingConditions()
+global mode w p;
+if p.teachingModeOn
+    mode.teach = true; % the teacher is willing to teach at the begining
+end
+% check if it is behave correctly
+if p.teacherForcingOn && mode.teacherForcing
+    fprintf('.')
+    if w.nItems + 1 ~= w.stateNum
+        warning('?')
+    end
+end
+end
+
+function letTheModelTryAgain()
+global w mode;
+fprintf('.');
+% re-initialize the world if REDO
+reinitState();
+updateState();
+w.maxTeachTrial = w.maxTeachTrial - 1;
+if w.maxTeachTrial == 0
+    % teacher give up if the model don't learn in 100 iterations
+    mode.teach = false;
+end
+end
---
+++ sim16.5.2_hidden/updateWeights.m
@@ -0,0 +1,32 @@
+% written by professor Jay McClelland
+function [ ] = updateWeights()
+% this function controls:
+% 1. the reward policy
+% 2. the weight update
+% 3. activate the "teaching"
+global p a w;
+%% compute the reward values according to the reward policy
+curRwd = computeRwd();
+a.aAct_next = a.wts_HA*sigmoid(a.wts_VH * w.vS.visInput');
+a.aAct_next(p.mvRad + 1) = a.aAct_next(p.mvRad + 1) + a.bias;
+expRwd = max(a.aAct_next); 
+
+% there is no expected reward for the last step
+if ~w.done
+    a.dfRwd = curRwd + expRwd * p.gamma;
+else
+    a.dfRwd = curRwd; 
+end
+
+%% update the weights - back prop
+% compute delta the active action unit and hidden units
+delta3 = a.dfRwd - a.aAct(a.choice); 
+delta2 = a.wts_HA(a.choice, :)' * delta3 .* (a.hAct.*(1-a.hAct));
+
+% compute the changes for the weights
+wtsHA_change = delta3 * a.hAct';
+wtsVH_change = delta2 * w.vS.oldInput;
+% update the weights
+a.wts_HA(a.choice,:) = a.wts_HA(a.choice,:) + p.lrate * wtsHA_change;
+a.wts_VH = a.wts_VH + p.lrate * wtsVH_change;
+end
