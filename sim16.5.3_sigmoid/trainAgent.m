924ac728e66f163cd39b21c3e282bb889a6ae9b4
Qihong Lu <lvqihong1992@gmail.com>
2015-09-03 22:16:08 -0500

    fix completion recording

--- sim16.5.3_sigmoid/trainAgent.m
+++ sim16.5.3_sigmoid/trainAgent.m
@@ -36,7 +36,7 @@ for i = 1:p.runs
     s.steps(i) = result.steps;
     s.indices{i} = result.indices;
     s.numItemsShowed(i) = w.nItems;
-    if w.nItems == length(getNonzeros(s.indices{i}))
+    if all(w.rS.targRemain==false) && i ~= p.maxIter
         s.completed(i) = true;
     end
 end
