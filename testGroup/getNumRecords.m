54fdef5ecd282b3b91379490f643ee339ce301d0
Qihong Lu <lvqihong1992@gmail.com>
2015-09-26 16:46:38 -0500

    functions that figure out how many records there are

---
+++ testGroup/getNumRecords.m
@@ -0,0 +1,27 @@
+%% This function counts how many records you have
+% It counts number of files that are in the form of "recordXX.mat", where
+% XX is the index of the record. Based on this, it will be able to figure
+% out the number of subejcts, which is convenient for running analysis. 
+function [numSubjects] = getNumRecords(path)
+
+% initialize the counter
+numSubjects = 0;
+
+% read all file names
+matFiles = dir([path '/*.mat']);
+numFiles = length(matFiles);
+expression = 'record[0-9][0-9].mat';
+
+% loop over all files
+for i = 1 : numFiles
+    tempStr = matFiles(i).name;
+    % check if the file is in the form of "recordXX.mat"
+    temp = regexp(tempStr, expression,'match');
+    % increment numSubject if YES
+    if(isempty(temp))
+        disp(tempStr);
+    else
+        numSubjects = numSubjects + 1;
+    end
+end
+end
