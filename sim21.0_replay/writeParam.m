ea4ec63fb355901603aaf7120a2b2cfdb42a3773
Qihong Lu <lvqihong1992@gmail.com>
2016-07-13 16:52:13 -0700

    handle additional cases

--- sim21.0_replay/writeParam.m
+++ sim21.0_replay/writeParam.m
@@ -7,12 +7,23 @@ fields = fieldnames(param);
 for i=1:numel(fields)
     fieldName = fields{i};
     fieldVal = param.(fields{i});
+
     if isnumeric(fieldVal)
         fieldVal = num2str(fieldVal);
+
+    elseif islogical(fieldVal)
+        if fieldVal
+            fieldVal = 'true';
+        else
+            fieldVal = 'false';
+        end
+        
     elseif isstruct(fieldVal)
-        continue; 
+        % if this filed is a structure, skip it
+        % too lazy to implement it, use recursive alg.
+        continue;
     end
-    % write parameter info to txt 
+    % write parameter info to txt
     fprintf(fileID,'%.20s %4s\n',fieldName, fieldVal);
 end
 
