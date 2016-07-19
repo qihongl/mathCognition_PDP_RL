ae970eccd35816decf9ebeb8c1abc9d49fd0c8c9
Qihong Lu <lvqihong1992@gmail.com>
2016-07-19 12:24:47 -0700

    sep visualize wts function

--- sim21.0_replay/showWeights.m
+++ sim21.0_replay/showWeights.m
@@ -10,9 +10,9 @@ xlabel(d.heatWts, 'Visual input layer', 'fontsize', d.FONTSIZE)
 ylabel(d.heatWts, 'Action layer', 'fontsize', d.FONTSIZE)
 end
 
-function visualizeWeightsMatrix(wts)
-global p;
-imagesc(-p.eyeRad:p.eyeRad,-p.mvRad:p.mvRad+1, wts)
-set(gca,'xtick',-p.eyeRad:1:p.eyeRad);
-set(gca,'ytick',-p.mvRad:1:p.mvRad+1);
-end
+% function visualizeWeightsMatrix(wts)
+% global p;
+% imagesc(-p.eyeRad:p.eyeRad,-p.mvRad:p.mvRad+1, wts)
+% set(gca,'xtick',-p.eyeRad:1:p.eyeRad);
+% set(gca,'ytick',-p.mvRad:1:p.mvRad+1);
+% end
