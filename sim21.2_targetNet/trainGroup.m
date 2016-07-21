6a9bcd418ff23302f4f8e90103042d8a82ddb579
Qihong Lu <lvqihong1992@gmail.com>
2016-07-21 10:04:29 -0700

    update global parameter for every run

--- sim21.2_targetNet/trainGroup.m
+++ sim21.2_targetNet/trainGroup.m
@@ -1,23 +1,24 @@
 function [] = trainGroup(nSubj, epoch)
 clear global; 
 % Run N models, to establish sample size
-global p;
 dataSaveDirName = 'groupData';
 projDir = fullfile(pwd, dataSaveDirName);
-simName = 'tryTargNet';
+simName = 'ep2000_freq2000';
 param.saveDir = fullfile(projDir, simName);
 
 %% set the parameter that you want to vary
 if nargin == 0
-    epoch = 5000;
+    epoch = 2000;
     nSubj = 20;
 end
 
-param.runningCondition = 'fix teaching mode, varying replay buffer parameter';
+param.runningCondition = 'NA';
 % param.val = [1 2 3 4];
-param.val = [16 32];
+param.val = [5 10 30];
 % param.val = 0
 for i = 1 : length(param.val)
+    
+    global p;
     %     p.teachingStyle = param.val(i); % varying parameter
     p.replay_batchSize = param.val(i);
     
