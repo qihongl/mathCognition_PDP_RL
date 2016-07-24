4856bcd3b7b00c42690058421adff0b13e1d10dc
Qihong Lu <lvqihong1992@gmail.com>
2016-07-23 20:18:51 -0700

    save current reward without passing arg back

--- shared/computeRwd.m
+++ shared/computeRwd.m
@@ -6,7 +6,7 @@ w.actionCorrect = true;
 if targetRemain()
     if a.choice == p.mvRange +1     % saying "done"
         Rwd = p.r.smallNeg;
-        w.errors = w.errors + 1; 
+        w.errors = w.errors + 1;
         w.done = true;
     elseif a.choice == p.mvRad +1     % not moving
         Rwd = p.r.smallNeg;
@@ -14,28 +14,30 @@ if targetRemain()
         Rwd = p.r.smallNeg;
     elseif objIsTouched         % touching a previously touched object
         Rwd = p.r.midNeg;
-        w.errors = w.errors + 1; 
+        w.errors = w.errors + 1;
         w.numDoubleTouch = w.numDoubleTouch +1;
         w.actionCorrect = false;
     elseif ~isNext  % not touching left most untouched obj
         Rwd = p.r.midNeg;
         w.actionCorrect = false;
-        w.errors = w.errors + 1; 
+        w.errors = w.errors + 1;
         w.numSkips = w.numSkips+1;
         w.rS.targRemain(w.rS.handPos == w.rS.targPos) = false;
     else            % CORRECT: touching left most untouched obj
         Rwd = p.r.midPos;
         w.rS.targRemain(w.rS.handPos == w.rS.targPos) = false;
         
-        % update touched locations 
-%         w.rS.touchedLocs_old = w.rS.touchedLocs_cur; 
-%         w.rS.touchedLocs_cur = false(1, p.eyeRange);
-%         observableTrags = abs(w.vS.targPos) < p.eyeRad;
-%         w.rS.touchedLocs_cur(w.vS.targPos(observableTrags) + p.eyeRad) = ~w.rS.targRemain(observableTrags);
-%         % update the input 
-%         w.input_old = horzcat(w.vS.oldInput, w.rS.touchedLocs_old); 
-%         w.input_cur = horzcat(w.vS.visInput, w.rS.touchedLocs_cur); 
-%         temp = 0; 
+%         if isfield(p,'sensoryInput') && p.sensoryInput
+%             % update touched locations
+%             w.rS.touchLocs_old = w.rS.touchLocs_cur;
+%             w.rS.touchLocs_cur = false(1, p.eyeRange);
+%             observableTrags = abs(w.vS.targPos) < p.eyeRad;
+%             w.rS.touchLocs_cur(w.vS.targPos(observableTrags) + p.eyeRad) = ~w.rS.targRemain(observableTrags);
+%             % update the input
+%             w.input_old = horzcat(w.vS.oldInput, w.rS.touchLocs_old);
+%             w.input_cur = horzcat(w.vS.visInput, w.rS.touchLocs_cur);
+%             
+%         end
     end
 else    % if all targets were touched
     if a.choice == p.mvRange +1
@@ -44,8 +46,11 @@ else    % if all targets were touched
     else
         Rwd = p.r.smallNeg;
     end
+    
 end
 
+
+a.curRwd = Rwd; 
 end % end of the function
 
 
