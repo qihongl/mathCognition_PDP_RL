daf4b2dedd18ee6f531b5acccfc492cf908d4ed9
Qihong Lu <lvqihong1992@gmail.com>
2016-07-23 21:47:41 -0700

    generalize it

--- shared/testInput.m
+++ shared/testInput.m
@@ -1,10 +1,6 @@
-function [] = testInput()
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-global w p
-if any(w.input_cur(1:p.eyeRange) ~= w.vS.visInput_cur)
-    error();
+function [] = testInput(x,y)
+if any(x ~= y)
+    error('THE TWO INPUTS ARE DIFFERENT.');
 end
-
 end
 
